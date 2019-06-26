026e9a194ec9ad4833feac7a61ca3d6394f7f381
Tadas O <tadaso@wix.com>
2019-06-26 18:28:20 +0300

    Add delete button

--- cvbuilder/react/src/app/components/Contacts/index.js
+++ cvbuilder/react/src/app/components/Contacts/index.js
@@ -3,18 +3,26 @@ import './index.css';
 import {useFetch} from "../../utils/fetch-hook";
 import {API_ENDPOINTS} from "../../utils/constants";
 import {FaEnvelope, FaGithubAlt, FaLinkedinIn, FaMapMarker, FaPhone} from 'react-icons/fa';
-import {API_DOMAIN} from "../../utils/constants";
+import {post} from "../../utils/post";
 
 const icons = {FaEnvelope, FaPhone, FaMapMarker, FaLinkedinIn, FaGithubAlt};
 
 function Item(data) {
   const Icon = icons[data.type];
 
+  const clicked = event => {
+    const {value} = event.target;
+    console.log(value)
+  };
+
   return (
-    <a className="Contacts--item" href={data.url}>
-      <Icon className="Contacts--item-icon"/>
-      <span className="Contacts--item-text">{data.value}</span>
-    </a>
+    <div>
+      <a className="Contacts--item" href={data.url}>
+        <Icon className="Contacts--item-icon"/>
+        <span className="Contacts--item-text">{data.value}</span>
+      </a>
+      <button  value={data.id} onClick={clicked}>Delete</button>
+    </div>
   );
 }
 
@@ -51,32 +59,18 @@ function AddContactForm(props) {
 
   const {loading, data} = useFetch(API_ENDPOINTS.contactsTypes);
 
-  // const createContact = async event => {
-  //   event.preventDefault();
-  //   if (!contact.value || !contact.type || !contact.url) return;
-  //
-  //   const response = await fetch(`${API_DOMAIN}${API_ENDPOINTS.addContact}`,
-  //     {
-  //       body: JSON.stringify(contact), method: 'POST', headers: {
-  //         'Accept': 'application/json',
-  //         'Content-Type': 'application/json'
-  //       }
-  //     }).then(resp => resp.json());
-  //
-  //   props.addContact(response);
-  //   setContact(initialFormState)
-  // };
+  const createContact = async event => {
+    event.preventDefault();
+    if (!contact.value || !contact.type || !contact.url) return;
+
+    const response = await post(API_ENDPOINTS.addContact, contact);
+
+    props.addContact(response);
+    setContact(initialFormState)
+  };
 
   return (
-    <form
-      onSubmit={event => {
-        event.preventDefault();
-        if (!contact.value || !contact.type || !contact.url) return;
-
-        props.addContact(contact);
-        setContact(initialFormState)
-      }}
-    >
+    <form onSubmit={createContact}>
       <label>Value</label>
       <input type="text" name="value" value={contact.value} onChange={handleInputChange}/>
       <label>Type</label>
